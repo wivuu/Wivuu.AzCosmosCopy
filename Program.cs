e42d1ed09215ecf7e9b02ca8d716dbea726b3b87
Erik OLeary <erik.m.oleary@gmail.com>
2020-12-03 12:57:43 -0600

    Change how args are validated

--- Program.cs
+++ Program.cs
@@ -1,55 +1,44 @@
-﻿using System;
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Invocation;
 using Microsoft.Azure.Cosmos;
 
 var root = new RootCommand
 {
     new Option<string?>(
-        new [] { "-s", "--source" }, "Source connection string"
+        new [] { "-s", "--source" }, "Source connection string (required)"
+    ),
+
+    new Option<string?>(
+        new [] { "--sd", "--source-database" }, "Source database name (required)"
     ),
     
     new Option<string?>(
         new [] { "-d", "--destination" }, "Destination connection string"
     ),
-
-    new Option<string?>(
-        new [] { "--sn", "--source-database" }, "Source database name"
-    ),
     
     new Option<string?>(
-        new [] { "--dn", "--destination-database" }, "Destination database name"
+        new [] { "--dd", "--destination-database" }, "Destination database name"
     ),
 };
 
+// Add validators
+root.AddValidator(result => result.Children.Contains("-s") ? default : "--source is required");
+
+root.AddValidator(result => result.Children.Contains("--sd") ? default : "--source-database is required");
+
+root.AddValidator(result => 
+    !result.Children.Contains("-d") && !result.Children.Contains("--dd")
+    ? "--destination must be specified if --destination-database is not" : default
+);
+
 root.Description = "Copy Cosmos database from source to destination";
 
+// Handle command
 root.Handler = CommandHandler.Create(
-    async (string source, string? destination, string sourceDatabase, string? destinationDatabase) =>
+    async (Args args) =>
     {
-        if (source is null)
-        {
-            Console.WriteLine("--source is required");
-            return 1;
-        }
-
-        if (sourceDatabase is null)
-        {
-            Console.WriteLine("--source-database must be specified");
-            return 2;
-        }
-
-        if (destination is null && destinationDatabase is null)
-        {
-            Console.WriteLine("--destination must be specified if --destination-database is not");
-            return 3;
-        }
-        
-        if (destination is null && destinationDatabase == sourceDatabase)
-        {
-            Console.WriteLine("--destination cannot be copied to --source with the same --destination-database name");
-            return 4;
-        }
+        if (args.Destination is null && args.DestinationDatabase == args.SourceDatabase)
+            throw new System.Exception("--destination cannot be copied to --source with the same --destination-database name");
 
         var options = new CosmosClientOptions 
         { 
@@ -57,11 +46,19 @@ root.Handler = CommandHandler.Create(
             AllowBulkExecution = true,
         };
 
-        var sourceClient = new CosmosClient(source, options);
-        var destClient   = new CosmosClient(destination ?? source, options);
+        var sourceClient = new CosmosClient(args.Source, options);
+        var destClient   = new CosmosClient(args.Destination ?? args.Source, options);
 
-        return await DbCopier.CopyAsync(sourceClient, destClient, sourceDatabase, destinationDatabase ?? sourceDatabase)
-            ? 0 : 5;
+        return await DbCopier.CopyAsync(sourceClient, destClient, args.SourceDatabase, args.DestinationDatabase ?? args.SourceDatabase)
+            ? 0 : 1;
     });
 
 await root.InvokeAsync(args);
+
+class Args
+{
+    public string Source { get; init; } = default!;
+    public string SourceDatabase { get; init; } = default!;
+    public string? Destination { get; init; }
+    public string? DestinationDatabase { get; init; }
+}
